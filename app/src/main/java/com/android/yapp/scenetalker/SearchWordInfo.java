96992376866a1d5a5bca5a01285b8b390295f73b
SimJunSik <wnstlr24@naver.com>
2019-12-24 17:50:18 +0900

    북마크 이미지 등등 추가

---
+++ app/src/main/java/com/android/yapp/scenetalker/SearchWordInfo.java
@@ -0,0 +1,18 @@
+package com.android.yapp.scenetalker;
+
+public class SearchWordInfo {
+
+    private String search_word;
+
+    public SearchWordInfo(String search_word) {
+        this.search_word = search_word;
+    }
+
+    public String getSearch_word() {
+        return search_word;
+    }
+
+    public void setSearch_word(String search_word) {
+        this.search_word = search_word;
+    }
+}
