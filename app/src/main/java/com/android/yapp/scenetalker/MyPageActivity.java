ff8cf5645da425a0289589146056c24dba0cfa24
SimJunSik <wnstlr24@naver.com>
2020-01-05 17:58:17 +0900

    MainActivity에서 권한 설정 추가

--- app/src/main/java/com/android/yapp/scenetalker/MyPageActivity.java
+++ app/src/main/java/com/android/yapp/scenetalker/MyPageActivity.java
@@ -1,13 +1,16 @@
 package com.android.yapp.scenetalker;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -15,7 +18,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -66,11 +71,20 @@ public class MyPageActivity extends AppCompatActivity {
     ImageButton profile_img_change;
 
     Context context;
+
+    String[] permission_list = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_page);
         context = getApplicationContext();
+
+        checkPermission();
 
         mypageback = findViewById(R.id.mypage_back_btn);
         mypageback.setOnClickListener(new View.OnClickListener() {
@@ -258,6 +272,40 @@ public class MyPageActivity extends AppCompatActivity {
             });
 
             Glide.with(context).load(selectedImageUri).error(R.drawable.default_image).into(mypage_profile_image);
+        }
+    }
+
+    public void checkPermission(){
+        //현재 안드로이드 버전이 6.0미만이면 메서드를 종료한다.
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return;
+
+        for(String permission : permission_list){
+            //권한 허용 여부를 확인한다.
+            int chk = checkCallingOrSelfPermission(permission);
+
+            if(chk == PackageManager.PERMISSION_DENIED){
+                //권한 허용을여부를 확인하는 창을 띄운다
+                requestPermissions(permission_list,0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==0)
+        {
+            for(int i=0; i<grantResults.length; i++)
+            {
+                //허용됬다면
+                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"앱권한설정하세요",Toast.LENGTH_LONG).show();
+                    // finish();
+                }
+            }
         }
     }
 }
